29254a24fb104f56d7ed4ff7f784796c86e55c8a
coincard2m <thanh.trannhomthanhbinh2012@gmail.com>
2026-09-22 21:32:23 +0700

    Update ralink_nand.c

--- trunk/linux-3.4.x/drivers/mtd/ralink/ralink_nand.c
+++ trunk/linux-3.4.x/drivers/mtd/ralink/ralink_nand.c
@@ -2363,12 +2363,6 @@ static int __init ra_nand_init(void)
 	int alloc_size, bbt_size, buffers_size;
 #if !defined (CONFIG_MTD_NAND_USE_UBI_PART)
 	uint32_t kernel_size = 0x200000;
-#if defined (CONFIG_RT2880_ROOTFS_IN_FLASH) && defined (CONFIG_ROOTFS_IN_FLASH_NO_PADDING)
-	_ihdr_t hdr;
-	loff_t offs;
-	size_t ret_len = 0;
-	int check;
-#endif
 #endif
 
 	if (ra_check_flash_type() != BOOT_FROM_NAND)
@@ -2448,38 +2442,11 @@ static int __init ra_nand_init(void)
 	mutex_init(ra->controller);
 
 #if !defined (CONFIG_MTD_NAND_USE_UBI_PART)
-#if defined (CONFIG_RT2880_ROOTFS_IN_FLASH) && defined (CONFIG_ROOTFS_IN_FLASH_NO_PADDING)
-	/* 1. Kiem tra PB-Boot truoc tai vi tri 0x200000 (2MB) */
-	offs = 0x200000;
-	check = offs;
-	while (nand_block_checkbad(ra, check)) {
-		check += ranfc_mtd->erasesize;
-	}
-	memset(&hdr, 0, sizeof(hdr));
-	ramtd_nand_read(ranfc_mtd, offs, sizeof(hdr), &ret_len, (u_char *)(&hdr));
+	/* Set truc tiep offset 0x200000 cho PB-Boot Mi Router 3 */
+	rt2880_partitions[NAND_MTD_KERNEL_PART_IDX].offset = 0x200000;
+	rt2880_partitions[ARRAY_SIZE(rt2880_partitions) - 1].offset = 0x200000;
+	kernel_size = 0x200000;
 
-	/* Magic number chuan cua uImage la 0x27051956 */
-	if (ret_len == sizeof(hdr) && ntohl(hdr.ih_magic) == 0x27051956 && hdr.ih_ksz != 0) {
-		printk(KERN_INFO "ralink_nand: [AUTO-DETECT] PB-Boot layout detected at 0x200000!\n");
-		kernel_size = ntohl(hdr.ih_ksz);
-
-		/* Chinh lai offset Kernel va Firmware_Stub ve dung 0x200000 */
-		rt2880_partitions[NAND_MTD_KERNEL_PART_IDX].offset = 0x200000;
-		rt2880_partitions[ARRAY_SIZE(rt2880_partitions) - 1].offset = 0x200000;
-	} else {
-		/* 2. Neu khong thay tai 0x200000, fallback ve offset goc (Stock U-Boot) */
-		printk(KERN_INFO "ralink_nand: [AUTO-DETECT] Using stock layout at 0x%x\n", NAND_MTD_KERNEL_PART_OFFSET);
-		check = NAND_MTD_KERNEL_PART_OFFSET;
-		while (nand_block_checkbad(ra, check)) {
-			check += ranfc_mtd->erasesize;
-		}
-		offs = NAND_MTD_KERNEL_PART_OFFSET;
-		memset(&hdr, 0, sizeof(hdr));
-		ramtd_nand_read(ranfc_mtd, offs, sizeof(hdr), &ret_len, (u_char *)(&hdr));
-		if (ret_len == sizeof(hdr) && hdr.ih_ksz != 0)
-			kernel_size = ntohl(hdr.ih_ksz);
-	}
-#endif
 	/* calculate partition table */
 	recalc_partitions(ranfc_mtd->size, kernel_size);
 #else
